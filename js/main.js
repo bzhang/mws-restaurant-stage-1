85fcb16c57f52f2dfa0416f48a7d3409949460ca
Bingjun Zhang <bingjunzhang@gmail.com>
2018-10-20 17:05:25 -0700

    add aria attributes

--- js/main.js
+++ js/main.js
@@ -177,6 +177,8 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute('role', 'button');
+  more.setAttribute('aria-label', `View details about ${restaurant.name}`);
   li.append(more)
 
   return li
